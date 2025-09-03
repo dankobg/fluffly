4861f205f1614f647b3aa460d154c6fcd7da64b1
danko <dankopetrovic.bgd@gmail.com>
2025-09-03 19:41:24 +0200

    some animal schema

--- server/animal_handler.go
+++ server/animal_handler.go
@@ -2,26 +2,31 @@ package server
 
 import (
 	"context"
+	"fmt"
 
 	api "github.com/dankobg/fluffly/api/gen"
+	"github.com/dankobg/fluffly/dbg"
 )
 
 func (a *ApiHandler) CreateAnimal(ctx context.Context, request api.CreateAnimalRequestObject) (api.CreateAnimalResponseObject, error) {
-	panic("NOT IMPLEMENTED")
+	dbg.PrintJSON(request.Body)
+	return api.CreateAnimal201JSONResponse{}, nil
 }
 
 func (a *ApiHandler) UpdateAnimal(ctx context.Context, request api.UpdateAnimalRequestObject) (api.UpdateAnimalResponseObject, error) {
-	panic("NOT IMPLEMENTED")
+	dbg.PrintJSON(request.Body)
+	return api.UpdateAnimal201JSONResponse{}, nil
 }
 
 func (a *ApiHandler) DeleteAnimal(ctx context.Context, request api.DeleteAnimalRequestObject) (api.DeleteAnimalResponseObject, error) {
-	panic("NOT IMPLEMENTED")
+	fmt.Println(request.ID)
+	return api.DeleteAnimal204Response{}, nil
 }
 
 func (a *ApiHandler) ListAnimals(ctx context.Context, request api.ListAnimalsRequestObject) (api.ListAnimalsResponseObject, error) {
-	panic("NOT IMPLEMENTED")
+	return api.ListAnimals200JSONResponse{}, nil
 }
 
 func (a *ApiHandler) GetAnimal(ctx context.Context, request api.GetAnimalRequestObject) (api.GetAnimalResponseObject, error) {
-	panic("NOT IMPLEMENTED")
+	return api.GetAnimal200JSONResponse{}, nil
 }
